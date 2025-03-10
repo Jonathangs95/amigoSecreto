c82ecc118b6d67162dc976a1f58d17879568f324
Jonathan Gonçalves <jhown@outlook.com>
2025-03-09 23:50:16 -0300

    Adicionado primeiras funções JS

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+  let nome = document.querySelector("input").value;
+  console.log(nome);
+  if (nome == "") {
+    alert("Nome em branco, favor preencher um nome valido");
+  } else {
+    limparCampo();
+  }
+}
+
+function limparCampo() {
+  nome = document.querySelector("input");
+  nome.value = "";
+}
+
+adicionarAmigo();
